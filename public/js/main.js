ca804d8e8d234ddab06450d98b0cba9c336a751e
asakuracl <champi.ludo@hotmail.fr>
2020-01-19 19:39:29 -0300

    update to poo

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+/*
 window.onscroll = function () { myFunction() };
 
 var navbar = document.getElementById("navbar");
@@ -10,3 +11,27 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+*/
+
+class moveEffect {
+    constructor(navbar) {
+        this.navbar = navbar;
+        this.sticky = navbar.offsetTop;
+        this.window = window;
+
+        //listener
+        this.window.addEventListener("scroll", this.stickyScroll.bind(this));
+    }
+
+    stickyScroll() {
+        if (this.window.pageYOffset >= this.sticky) {
+            this.navbar.classList.add("sticky")
+        } else {
+            this.navbar.classList.remove("sticky");
+        }
+    }
+}
+
+const navbar = document.getElementById("navbar");
+
+const move = new moveEffect(navbar);
