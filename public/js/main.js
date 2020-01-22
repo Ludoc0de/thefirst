bc128e4e2883d2304d427c794243843a9426801f
asakuracl <champi.ludo@hotmail.fr>
2020-01-22 14:30:05 -0300

    remove comment

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,3 @@
-/*
-window.onscroll = function () { myFunction() };
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
-*/
-
 class moveEffect {
     constructor(navbar) {
         this.navbar = navbar;
