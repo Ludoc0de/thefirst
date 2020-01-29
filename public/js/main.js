699b7953efa20eb695c0cd9168dafc377a8612d1
asakuracl <champi.ludo@hotmail.fr>
2020-01-29 17:06:50 -0300

    add this.navbarColors to get all navbar-span-1. Creat a loop in navbarChange to change color

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@ class moveEffect {
     constructor() {
         this.navbarLi = document.getElementsByClassName("navbar-li");
         this.window = window;
+        this.navbarColors = document.getElementsByClassName("navbar-span-1");
+        console.log(this.navbarColor);
 
         //listener
         this.window.addEventListener("scroll", this.navbarChange.bind(this));
@@ -13,6 +15,14 @@ class moveEffect {
                 navbarLi.classList.add("shrink");
             } else {
                 navbarLi.classList.remove("shrink");
+            }
+        }
+
+        for (let navbarColor of this.navbarColors) {
+            if (document.documentElement.scrollTop > 50) {
+                navbarColor.style.backgroundColor = "#FF8A00";
+            } else {
+                navbarColor.style.backgroundColor = "orange";
             }
         }
     }
