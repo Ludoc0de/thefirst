db8a4d3b54ec57e8201e8f2e9328cecbc0598680
asakuracl <champi.ludo@hotmail.fr>
2020-01-29 19:43:48 -0300

    creat const minus to get .fa-plus. In condition add/remove/replace minus

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -2,12 +2,18 @@ const accordion = document.querySelector(".accordion");
 
 const postView = document.querySelector(".postView-comment-div");
 
+const minus = document.querySelector(".fa-plus");
+
 accordion.addEventListener("click", function () {
     accordion.classList.toggle('is-open');
 
     if (accordion.classList.contains('is-open')) {
         postView.style.height = postView.scrollHeight + "px";
+        minus.classList.remove("fa-plus");
+        minus.classList.add("fa-minus");
     } else {
         postView.style.height = '0';
+        minus.classList.remove("fa-minus");
+        minus.classList.add("fa-plus");
     }
 })
