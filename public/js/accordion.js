1f03058b3924a93a5eebea318acf995a4745fc2f
asakuracl <champi.ludo@hotmail.fr>
2020-01-29 19:37:32 -0300

    update listener. postview.classList to accordion.classList Add condition in listener to open and close comments

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -2,7 +2,12 @@ const accordion = document.querySelector(".accordion");
 
 const postView = document.querySelector(".postView-comment-div");
 
-
 accordion.addEventListener("click", function () {
-    postView.classList.toggle('is-open')
+    accordion.classList.toggle('is-open');
+
+    if (accordion.classList.contains('is-open')) {
+        postView.style.height = postView.scrollHeight + "px";
+    } else {
+        postView.style.height = '0';
+    }
 })
