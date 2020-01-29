b37d889caeb89ce902d1d08d02137b3912623aff
asakuracl <champi.ludo@hotmail.fr>
2020-01-28 23:04:38 -0300

    some clean, work on navbar effect

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,15 @@
 class moveEffect {
     constructor(navbar) {
         this.navbar = navbar;
-        this.sticky = navbar.offsetTop;
         this.window = window;
 
         //listener
-        this.window.addEventListener("scroll", this.stickyScroll.bind(this));
+        this.window.addEventListener("scroll", navbarChange.bind(this));
     }
 
-    stickyScroll() {
-        if (this.window.pageYOffset >= this.sticky) {
-            this.navbar.classList.add("sticky")
+    navbarChange() {
+        if (this.window.pageYOffset >= this.navbar) {
+            this.navbar.style.("sticky")
         } else {
             this.navbar.classList.remove("sticky");
         }
