2e26ff0879eb903b01cfc96a19ae8279ae94c647
asakuracl <champi.ludo@hotmail.fr>
2020-01-30 23:46:14 -0300

    update some class name

--- view/backend/updatePostView.php
+++ view/backend/updatePostView.php
@@ -7,22 +7,26 @@
 ?>
 
 <section class="updatePost-section">
-    <div>
-        <h1 class="mx-auto">Modifier les chapitres</h1>
+    <div class="updatePost-first-div">
+        <h1>
+            Modifier les chapitres
+        </h1>
+
+        <form action="index.php?action=updatePost&amp;id=<?=$post['id'];?>" method="post">
+            <div class="updatePost-form">
+                <label for="title">Titre</label><br>
+                <input type="text" id="title" name="title" class="updatePost-input" value="<?=$post['title'];?>" />
+                <br>
+                <label for="newPost">Contenu</label><br>
+                <textarea id="newPost" class="updatePost-textarea" name="content"
+                    rows="5"><?=$post['content'];?></textarea>
+            </div>
+            <div>
+                <input type="submit" class="updatePost-button" value="Modifier"
+                    onclick="return confirm('Confirmez-vous la modification ?')" />
+            </div>
+        </form>
     </div>
-    <form action="index.php?action=updatePost&amp;id=<?=$post['id'];?>" method="post">
-        <div class="updatePost-form">
-            <label for="title">Titre</label><br>
-            <input type="text" id="title" name="title" class="updatePost-input" value="<?=$post['title'];?>" />
-            <br>
-            <label for="newPost">Contenu</label><br>
-            <textarea id="newPost" class="updatePost-textarea" name="content" rows="5"><?=$post['content'];?></textarea>
-        </div>
-        <div>
-            <input type="submit" class="btn btn-dark mt-1" value="Modifier"
-                onclick="return confirm('Confirmez-vous la modification ?')" />
-        </div>
-    </form>
     <p class="col-lg-8 mx-auto">
         <a href="index.php?action=erasePost" class="text-dark">Retour</a>
     </p>
