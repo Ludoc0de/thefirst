f44818bb29c970735d17ec5a387b4024742a5d91
asakuracl <champi.ludo@hotmail.fr>
2020-02-01 11:48:16 -0300

    add id in login-form

--- view/frontend/login.php
+++ view/frontend/login.php
@@ -21,8 +21,8 @@
     <form class="login-form" action="/thefirst/index.php?action=loginPage" method="post">
         <p>Se connecter ? C'est ici !</p>
         <div>
-            <input class="login-input" type="text" name="nickname" placeholder="nom d'utilisateur">
-            <input class="login-input" type="password" name="pass" placeholder="mot de passe">
+            <input id="login-pseudo" class="login-input" type="text" name="nickname" placeholder="nom d'utilisateur">
+            <input id="login-pass" class="login-input" type="password" name="pass" placeholder="mot de passe">
         </div>
         <button type="submit" class="login-button"> Se connecter</button>
     </form>
