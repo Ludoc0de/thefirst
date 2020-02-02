41f5a088de2fda8e697b6f7299057ca373e8dd45
asakuracl <champi.ludo@hotmail.fr>
2020-02-02 10:21:29 -0300

    add tinymce in template frontend

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -180,6 +180,14 @@
     <script src="/thefirst/public/js/main.js"></script>
     <script src="/thefirst/public/js/formComment.js"></script>
     <script src="/thefirst/public/js/formLogin.js"></script>
+    <script src="https://cdn.tiny.cloud/1/xcs5ihxcsh4gaznurwzojymkulh3h9cd8evxurucg20opu9z/tinymce/5/tinymce.min.js">
+    </script>
+    <script>
+    tinymce.init({
+        selector: '.textareaTiny'
+
+    });
+    </script>
 </body>
 
 </html>
