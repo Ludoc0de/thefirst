eb1a28e296e7bc7743cb9f740ee34e703379026e
asakuracl <champi.ludo@hotmail.fr>
2020-02-04 11:54:20 -0300

    update form, switch tag element

--- view/backend/updatePostView.php
+++ view/backend/updatePostView.php
@@ -12,7 +12,21 @@
             Modifier les chapitres
         </h1>
 
-        <form action="index.php?action=updatePost&amp;id=<?=$post['id'];?>" method="post">
+        <form action="index.php?action=updatePost&amp;id=<?=$post['id'];?>" method="post" enctype="multipart/form-data">
+            <div class="radio">
+                <p class="radio-p">
+                    <?php if($post['draft'] > 0) {?>
+                    <span> Ceci est un brouillon !</span>
+                    <?php
+                    }
+                    ?>
+                    Votre choix, brouillon ?
+                </p>
+                <label for="oui">Oui</label>
+                <input type="radio" name="draft" id="draft-yes" value="1" />
+                <label for="non">Non</label>
+                <input type="radio" name="draft" id="draft-no" value="0" />
+            </div>
             <div class="updatePost-form">
                 <label for="title">Titre</label><br>
                 <input type="text" id="title" name="title" class="updatePost-input" value="<?=$post['title'];?>" />
@@ -21,20 +35,9 @@
                 <textarea id="newPost" class="updatePost-textarea textareaTiny" name="content"
                     rows="5"><?=$post['content'];?></textarea>
             </div>
-            <div class="radio">
-                <p class="radio-p">
-                    <?php if($post['draft'] > 0) {?>
-                    <span> Ceci est un brouillon !</span>
-                    <?php
-                    }
-                    ?>
-                    Est-ce toujours un brouillon ?
-                </p>
-                <label for="oui">Oui</label>
-                <input type="radio" name="draft" id="draft-yes" value="1" />
-                <label for="non">Non</label>
-                <input type="radio" name="draft" id="draft-no" value="0" />
-            </div>
+            <label for="title">Image titre</label><br>
+            <input class="createPost-image" type="file" id="title" name="view_image" value="<?=$post[4];?>" />
+            <br>
             <div>
                 <input type="submit" class="updatePost-button" value="Modifier"
                     onclick="return confirm('Confirmez-vous la modification ?')" />
